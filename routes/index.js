70e36ea09e241267c5fdc840c51642711e59e93a
KorJeongHyeonLee <nomar0107@hanyang.ac.kr>
2016-12-27 21:19:47 +0900

    Temp

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,20 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/index', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+router.post('/submit', function(req, res, next) {
+    res.render('submit', {req : req, res : res, id: req.body.id });
+});
+router.post('/teacher', function(req, res, next) {
+    res.render('teacher_main', {req : req, res : res, id: req.body.id });
+});
+router.post('/student', function(req, res, next) {
+    res.render('student_main', {req : req, res : res, id: req.body.id });
+});
+router.post('/parent', function(req, res, next) {
+    res.render('parent_main', {req : req, res : res, id: req.body.id });
+});
 
 module.exports = router;
