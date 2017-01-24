84555d2a05ee5b699284f63d60f2dbbf58010bd5
윤 지우 <wldnthsus@icloud.com>
2017-01-24 17:48:09 +0900

    [v0.0.3] asynchronous problem solved

--- public/javascripts/login/app.js
+++ public/javascripts/login/app.js
@@ -1,4 +1,5 @@
 // Initialize Firebase
+
 var student = function (s_id, s_name, s_school, s_phone, s_state) {
     this.s_id = s_id;
     this.s_name = s_name;
@@ -6,48 +7,70 @@ var student = function (s_id, s_name, s_school, s_phone, s_state) {
     this.s_phone = s_phone;
     this.s_state = s_state;
 };
+
+var ID;
+
 var s = new student();
-var login = angular.module('loginApp',  []);
+
+var login = angular.module('loginApp',  ['studentApp']);
 login.controller('loginController', ['$scope','$window', function ($scope,$window) {
-        $scope.student = true;
+    const studentRefObject = firebase.database().ref().child('student');
+    $scope.student = true;
         $scope.submit = function () {
-            var ID = $scope.ID;
+            ID = $scope.ID;
+            $scope.$broadcast('update_parent_controller', $scope.ID);
 
 
             if ($scope.student) {
-                const studentRefObject = firebase.database().ref().child('student');
 
                 //If exists, return true
                 studentRefObject.orderByChild("s_id").equalTo(ID).once("value", function(snapshot) {
                     var userData = snapshot.val();
                     if (userData===null){
                         $window.location.href= "indexError";
+                    }else {
+                        $window.location.href= "student";
                     }
 
                 });
-
-                //Ordering and finding
-                studentRefObject.orderByChild("s_id").equalTo(ID).on("child_added", function(snapshot) {
-
-                    s.s_name = snapshot.child('s_name').val();
-                    s.s_id = snapshot.child('s_id').val();
-                    s.s_phone = snapshot.child('s_phone').val();
-                    s.s_school = snapshot.child('s_school').val();
-                    s.state = snapshot.child('s_state').val();
-
-                });
-                console.log(s.s_name);
-                // $window.location.href= "student";
             }
         }
     }]);
 
+
+
 var studen = angular.module('studentApp', []);
-studen
-    .controller('introduceController', ['$scope', function ($scope) {
-         $scope.name = s.s_name;
-        $scope.id = s.s_id;
-        $scope.school = s.s_school;
-        $scope.phone = s.s_phone;
-        console.log(s.s_name);
-    }]);
+// var checking = function() {
+    studen
+        .controller('introduceController', ['$scope','$timeout', function ($scope,$timeout) {
+
+            const studentRefObject = firebase.database().ref().child('student');
+
+            var ID;
+
+            $scope.$on("update_parent_controller", function(event, message) {
+                 $scope.id = message;
+                 console.log($scope.id);
+            });
+
+            studentRefObject.orderByChild("s_id").equalTo("2014038122").on("child_added", function(snapshot) {
+                    s.s_name = snapshot.child('s_name').val();
+                    s.s_id = snapshot.child('s_id').val();
+                    s.s_phone = snapshot.child('s_phone').val();
+                    s.s_school = snapshot.child('s_school').val();
+                    s.s_state = snapshot.child('s_state').val();
+
+                // $scope.name = $firebaseArray(studentRefObject);
+                $timeout(function() {
+                    $scope.name = s.s_name;
+                    $scope.id = s.s_id;
+                    $scope.phone = s.s_phone;
+                    $scope.school = s.s_school;
+
+                });
+            });
+
+
+
+        }]);
+
