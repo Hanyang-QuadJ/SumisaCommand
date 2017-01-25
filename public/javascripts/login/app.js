042787550d86bcc20ad7506bbbda63eec33fd6c2
윤 지우 <wldnthsus@icloud.com>
2017-01-25 18:50:51 +0900

    [v0.1.0] ng-cookies added & login complete

--- public/javascripts/login/app.js
+++ public/javascripts/login/app.js
@@ -12,48 +12,39 @@ var ID;
 
 var s = new student();
 
-var login = angular.module('loginApp',  ['studentApp']);
-login.controller('loginController', ['$scope','$window', function ($scope,$window) {
+var app = angular.module('loginApp', ['ngCookies']);
+//ngCookies Added
+app.controller('loginController', ['$scope','$cookies','$window', function ($scope,$cookies,$window) {
+
     const studentRefObject = firebase.database().ref().child('student');
-    $scope.student = true;
-        $scope.submit = function () {
+    $scope.submit = function () {
             ID = $scope.ID;
-            $scope.$broadcast('update_parent_controller', $scope.ID);
-
-
-            if ($scope.student) {
-
-                //If exists, return true
-                studentRefObject.orderByChild("s_id").equalTo(ID).once("value", function(snapshot) {
-                    var userData = snapshot.val();
-                    if (userData===null){
-                        $window.location.href= "indexError";
-                    }else {
-                        $window.location.href= "student";
-                    }
-
-                });
+            $cookies.put('ID',ID);
+        //If exists, return true
+        studentRefObject.orderByChild("s_id").equalTo(ID).once("value", function(snapshot) {
+            var userData = snapshot.val();
+            if (userData===null){
+                $window.location.href= "index";
+            }else {
+                $window.location.href= "student";
             }
-        }
-    }]);
+
+        });
+    }
+}]);
 
 
 
-var studen = angular.module('studentApp', []);
+
 // var checking = function() {
-    studen
-        .controller('introduceController', ['$scope','$timeout', function ($scope,$timeout) {
+    app
+        .controller('introduceController', ['$scope','$cookies','$timeout', function ($scope,$cookies,$timeout) {
 
             const studentRefObject = firebase.database().ref().child('student');
 
-            var ID;
 
-            $scope.$on("update_parent_controller", function(event, message) {
-                 $scope.id = message;
-                 console.log($scope.id);
-            });
 
-            studentRefObject.orderByChild("s_id").equalTo("2014038122").on("child_added", function(snapshot) {
+            studentRefObject.orderByChild("s_id").equalTo($cookies.get('ID')).on("child_added", function(snapshot) {
                     s.s_name = snapshot.child('s_name').val();
                     s.s_id = snapshot.child('s_id').val();
                     s.s_phone = snapshot.child('s_phone').val();
