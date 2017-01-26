4e68774f167bf633b1fa1e3b9c0c9551f5f6fb5b
KorJeongHyeonLee <nomar0107@hanyang.ac.kr>
2017-01-26 18:58:47 +0900

    [v0.1.0] test : bind sub-modules to kernel module

---
+++ app/app.module.js
@@ -0,0 +1,10 @@
+(function(){
+    'use strict';
+    angular.module('sumisaApp', [
+        'app.login',
+        'app.student',
+        'app.teacher',
+        'app.parent',
+        'app.admin'
+    ]);
+})();
